11769c9d7ff733bb63e6b46a3e2dee2a2b701bf2
jungdaehun1024 <pooh99102408@gmail.com>
2025-05-19 10:36:30 +0900

    refactor: 메인페이지 수정

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,9 +1,22 @@
-<template >
-   <h1>인덱스 페이지</h1>
+<template>
+   <div class="index-container">
+      <div class="text">인덱스 페이지</div>
+   </div>
+   
 
 </template>
 <script setup>
 
 </script>
-<style> 
+<style scoped> 
+   .index-container{
+         background-color: #0e0e0e;
+         height: 100vh;
+         .text{
+           padding-top: 20rem;
+            color: #ffffff;
+            text-align: center;
+            font-size: 15rem;
+         }
+   }
 </style>
