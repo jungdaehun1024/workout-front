d1f04b7edd3039af1f9ccce345ad5665e0912491
jungdaehun1024 <pooh99102408@gmail.com>
2025-05-19 10:32:55 +0900

    feat:전역 css 설정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
    <NavBar/>
- <RouterView/>
+ <RouterView />
 </template>
 <script setup>
 import NavBar from './components/NavBar.vue';
 </script>
+
 <style>
+  html{
+    font-size: 10px;
+   
+  }
+
+  body{
+      overflow-x: hidden;     
+      margin: 0;
+      background-color: #0e0e0e;
+  }
 </style>
