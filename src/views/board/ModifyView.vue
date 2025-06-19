8ca5f416b5cbf5e7cbe5d7186253590c48f5e546
jungdaehun1024 <pooh99102408@gmail.com>
2025-06-20 01:12:34 +0900

    refactor: 배경색 미적용-> 적용

--- src/views/board/ModifyView.vue
+++ src/views/board/ModifyView.vue
@@ -10,10 +10,10 @@
   <style lang="scss" scoped>
   .write-page {
     font-family: Arial, sans-serif;
-    max-width: 600px;
+    // max-width: 600px;
     margin: 0 auto;
     padding: 20px;
-    background-color: #f9f9f9;
+    background-color: #0a0a0a;
     border-radius: 8px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   }
