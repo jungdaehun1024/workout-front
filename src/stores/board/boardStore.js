72ea321537ecc044a2fdecdb7f6d54eb4ee35c60
jungdaehun1024 <pooh99102408@gmail.com>
2025-07-12 17:10:45 +0900

    refactor:글생성,수정 메소드 등록

--- src/stores/board/boardStore.js
+++ src/stores/board/boardStore.js
@@ -9,6 +9,7 @@ export const useBoardStore  = defineStore("board",()=>{
     const boards = ref([]); //게시글 목록
     const boardDetail = ref({}); // 게시글 상세 정보
     const boardsCount = ref(0);
+    const creatableCategories = ref([]);
 
     //Action
 
@@ -27,34 +28,67 @@ export const useBoardStore  = defineStore("board",()=>{
         }
     }
 
-    //불러오는 카테고리의 게시글 총 개수 
-    // async function getBoardTotalCount(categoryId,depth){
-    //     try{
-    //         const response = await axios.get(`/api/totalBoards`,{params:{
-    //             categoryId,
-    //             depth
-    //         },
-    //         withCredentials:true});
-
-    //         boardsCount.value = response.data.data;
-
-    //     }catch(err){
-    //         console.error(err.message);
-    //     }
-
-    // }
-
     //게시글 상세 가져오기 
     async function getBoardDetail(boardId) {
         try{
-            const response = await axios.get(`/api/getBoardDetail/${boardId}`); // 게시글 목록 가져오기
+            const response = await axios.get("/api/board-detail",{
+                params:{
+                    boardId,
+                },withCredentials:true
+            }); // 게시글 목록 가져오기
             boardDetail.value = response.data;
         }catch(err){
             console.error(err.message);
         }
     }
 
+    //게시글 생성 가능한 카테고리 목록
+    async function getCreatableCategories() {
+        try{
+            const response = await axios.get("/api/creatable-categories",{withCredentials:true});
+            creatableCategories.value = response.data.data
+        }catch(err){
+            console.error(err.message);
+        }
+        
+    }
+
+    //게시글 생성
+    async function postBoard(formData) {
+        try{
+          const response= await axios.post("/api/board",formData,{
+                headers:{
+                    "Content-Type" : "multipart/form-data",
+                },withCredentials:true
+            });
+
+            return response;
+        }catch(err){
+            console.error(err.message);
+        } 
+    }
+
+    //게시글 수정
+    async function putBoard(formData) {
+        try{
+           const response= await axios.put("/api/board",formData,{
+                headers:{
+                    "Content-Type" : "multipart/form-data",
+                },
+                withCredentials:true,
+            });
+
+            return response;
 
 
-    return {boardDetail,boards,boardsCount,getBoardDetail,getBoards}
+
+        }catch(err){
+            console.error(err.message);
+        }
+        
+    }
+
+
+
+    return {boardDetail,boards,boardsCount,creatableCategories,getBoardDetail,getBoards,getCreatableCategories,postBoard,putBoard}
 });
